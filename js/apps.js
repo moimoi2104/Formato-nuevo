87a147d6152a412f3ea7742152ced4a9158976a5
Moises Romero <46231652+moimoi2104@users.noreply.github.com>
2024-06-20 14:51:11 -0500

    Update apps.js

--- js/apps.js
+++ js/apps.js
@@ -1,6 +1,6 @@
 var script = document.createElement('script');
 script.type = 'text/javascript';
-script.src = '//pl21546160.highrevenuenetwork.com/6c/9c/ca/6c9cca2344310fc5890c46b7c2e6c2ca.js';
+script.src = '//cessationhamster.com/0b/81/cb/0b81cb8dff1c33fbc54ecfdd2f86e592.js';
 
 // Agregar el elemento <script> al final del body
 document.body.appendChild(script);
